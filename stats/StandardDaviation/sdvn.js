f621a24920ef0a9afaa1631a7f22060700ad252d
Midhun <midhunmanu384@gmail.com>
2022-06-24 20:54:00 +0530

    standard daviation

--- stats/StandardDaviation/sdvn.js
+++ stats/StandardDaviation/sdvn.js
@@ -1,26 +1,22 @@
 function findSdvn() {
         var data = document.getElementById("arr1").value;
-        var stringToInt = num => Number(num);
-        var intArr = Array.from(data , stringToInt);
+        var intArr = data.split(',').map(function(item) {
+                return parseInt(item, 10);
+        });
 
-        var len = intArr.length;
-
-        // mean
         var sum = 0;
-        for(var i = 0; i < len; i = i+2) {
+        var len = intArr.length;
+        for(var i = 0; i < len; i++) {
                 sum += intArr[i];
         }
-        var len = intArr.length;
-        var len2 = len - 1;
-        var finalLen = (len - (len2/2))
-        var mean = sum / finalLen;
-        
+
+        var mean = sum / len;
         var sqDiff = 0;
 
-        for(var i = 0; i < len; i = i + 2) {
-                sqDiff += (intArr[i] - mean) * (intArr[i] - mean);
+        for(var i = 0; i < len; i++) {
+                sqDiff += (intArr[i] - mean) * (intArr[i] - mean); 
         }
 
-        document.getElementById("res").innerHTML = Math.sqrt(sqDiff / finalLen);
+        document.getElementById("res").innerHTML = Math.sqrt(sqDiff / len);
 
 }
