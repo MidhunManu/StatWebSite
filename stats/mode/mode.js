9e319fe09da94e052e8d92c224fb56d214bc6467
Midhun <midhunmanu384@gmail.com>
2022-06-28 23:28:00 +0530

    statistics finished

--- stats/mode/mode.js
+++ stats/mode/mode.js
@@ -1,7 +1,11 @@
 function findMode() {
         var data = document.getElementById("arr1").value;
-        var stringToInt = num => Number(num);
-        var intArr = Array.from(data , stringToInt);
+        // var stringToInt = num => Number(num);
+        // var intArr = Array.from(data , stringToInt);
+
+        var intArr = data.split(',').map(function(item) {
+                return parseInt(item, 10);
+        });
 
         // console.log(typeof intArr[1]);
 
@@ -10,7 +14,7 @@ function findMode() {
         var res = intArr[0];
         var currCount = 1;
 
-        for(var i = 0; i < intArr.length; i = i + 2) {
+        for(var i = 0; i < intArr.length; i++) {
                 if(intArr[i] == intArr[i - 1]) {
                         currCount++;
                 }
